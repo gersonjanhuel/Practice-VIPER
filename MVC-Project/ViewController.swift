035d994f6bd81a4ab5c03bcd808f1c71a3917037
Gerson Janhuel <gersonjanhuelhuel@gmail.com>
2020-08-28 14:24:19 +0700

    Initial Commit

---
+++ MVC-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Project
+//
+//  Created by Gerson Janhuel on 28/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
